c6741df42e4b3f82d812865f4be038431ec753c8
ChiaYu <chiayu.dev@gmail.com>
2024-08-08 16:24:47 +0800

    update summary ui

---
+++ Android/app/src/main/java/com/example/note/adapter/SummaryAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.note.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.note.databinding.ViewCategorySummaryBinding
+import com.example.note.model.Note
+
+
+class SummaryAdapter(private val onDetailsClicked: ((String) -> Unit)? = null ): RecyclerView.Adapter<SummaryAdapter.ViewHolder>() {
+    private var data = listOf<Note>()
+
+    fun setNotes(notes: List<Note>) {
+        data = notes
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = data.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ViewCategorySummaryBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding, onDetailsClicked)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val note = data[position]
+        val shouldShowCategory = position == 0 || (data[position-1].category != note.category)
+        holder.bind(note, shouldShowCategory)
+    }
+
+    class ViewHolder(
+        private val binding: ViewCategorySummaryBinding,
+        private val onDetailsClicked: ((String) -> Unit)? = null
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(note: Note, showCategory: Boolean) {
+            binding.category.text = note.category
+            binding.headerView.visibility = if (showCategory) View.VISIBLE else View.GONE
+            binding.content.text = if (note.content.length > 20) {
+                "${note.content.take(20)}..."
+            } else {
+                note.content
+            }
+            binding.btnDetails.setOnClickListener {
+                onDetailsClicked?.invoke(note.category)
+            }
+        }
+    }
+
+}
--- Android/app/src/main/java/com/example/note/ui/summary/SummaryFragment.kt
+++ Android/app/src/main/java/com/example/note/ui/summary/SummaryFragment.kt
@@ -4,23 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.note.adapter.NotesAdapter
+import com.example.note.adapter.SummaryAdapter
 import com.example.note.databinding.FragmentSummaryBinding
 
 
 class SummaryFragment : Fragment() {
     private val viewModel: SummaryViewModel by viewModels { SummaryViewModel.Factory }
     private lateinit var binding: FragmentSummaryBinding
-    private lateinit var adapter: NotesAdapter //TODO: create new view and adapter later
+    private lateinit var adapter: SummaryAdapter //TODO: create new view and adapter later
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        adapter = NotesAdapter {
+        adapter = SummaryAdapter {
+            Toast.makeText(context, "$it clicked", Toast.LENGTH_SHORT).show()
         }
         binding = FragmentSummaryBinding.inflate(inflater)
         binding.summaryList.let {
